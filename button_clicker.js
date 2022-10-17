f09a37d6cd3bbdbc53488ddadbb6c06403e3b50c
tooba GG <dsereduk@gmail.com>
2022-10-17 11:18:06 -0700

    HTML, JS, CSS Practice: The Dojonary!

---
+++ button_clicker.js
@@ -0,0 +1,15 @@
+function logOut(element) {
+    if(element.innerText == "Login"){
+        element.innerText = "Logout";
+    } else {
+        element.innerText = "Login";
+    }
+}
+
+function message() {
+    alert("Ninja was liked!");
+}
+
+function hide(element) {
+    element.remove();
+}
